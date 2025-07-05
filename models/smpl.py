4dd57d7169ec05a398263594b646eac12901945f
Eisha Hemchand <47909626+hemchande@users.noreply.github.com>
2025-07-05 10:39:34 -0500

    Update smpl.py

--- models/smpl.py
+++ models/smpl.py
@@ -1,12 +1,68 @@
+
+
 import torch
 import numpy as np
 import smplx
 from smplx import SMPL as _SMPL
-from smplx.body_models import ModelOutput
+# from smplx.body_models import ModelOutput
 from smplx.lbs import vertices2joints
 
 import config
 import constants
+
+
+from typing import NewType, Union, Optional
+from dataclasses import dataclass, asdict, fields
+import numpy as np
+import torch
+
+Tensor = NewType('Tensor', torch.Tensor)
+Array = NewType('Array', np.ndarray)
+
+
+@dataclass
+class ModelOutput:
+    vertices: Optional[Tensor] = None
+    joints: Optional[Tensor] = None
+    full_pose: Optional[Tensor] = None
+    global_orient: Optional[Tensor] = None
+    transl: Optional[Tensor] = None
+    v_shaped: Optional[Tensor] = None
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+    def __iter__(self):
+        return self.keys()
+
+    def keys(self):
+        keys = [t.name for t in fields(self)]
+        return iter(keys)
+
+    def values(self):
+        values = [getattr(self, t.name) for t in fields(self)]
+        return iter(values)
+
+    def items(self):
+        data = [(t.name, getattr(self, t.name)) for t in fields(self)]
+        return iter(data)
+
+
+@dataclass
+class SMPLOutput(ModelOutput):
+    betas: Optional[Tensor] = None
+    body_pose: Optional[Tensor] = None
+
+
+@dataclass
+class SMPLHOutput(SMPLOutput):
+    left_hand_pose: Optional[Tensor] = None
+    right_hand_pose: Optional[Tensor] = None
+    transl: Optional[Tensor] = None
+
 
 class SMPL(_SMPL):
     """ Extension of the official SMPL implementation to support more joints """
@@ -24,10 +80,11 @@ class SMPL(_SMPL):
         extra_joints = vertices2joints(self.J_regressor_extra, smpl_output.vertices)
         joints = torch.cat([smpl_output.joints, extra_joints], dim=1)
         joints = joints[:, self.joint_map, :]
-        output = ModelOutput(vertices=smpl_output.vertices,
+        output = SMPLOutput(vertices=smpl_output.vertices,
                              global_orient=smpl_output.global_orient,
                              body_pose=smpl_output.body_pose,
                              joints=joints,
                              betas=smpl_output.betas,
                              full_pose=smpl_output.full_pose)
         return output
+
